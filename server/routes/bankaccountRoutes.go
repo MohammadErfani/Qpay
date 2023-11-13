91f900cefdab543ca52cf182533974e22262182a
aminzohrabifar <aminzf@gmail.com>
2023-11-13 21:03:20 +0330

    change bankaccount routing

--- server/routes/bankaccountRoutes.go
+++ server/routes/bankaccountRoutes.go
@@ -21,17 +21,17 @@ func BankAccountRoutes(bc *echo.Group) {
 		//if err != nil {
 		//	return c.JSON(http.StatusBadRequest, "Internal server error in create user ")
 		//}
-		account := &models.BankAccount{
-			UserID:       1,
-			BankID:       1,
-			Status:       0,
-			AccountOwner: "amin",
-			Sheba:        "IR0696000000010324200001",
-		}
+		//account := &models.BankAccount{
+		//	UserID:       1,
+		//	BankID:       1,
+		//	Status:       0,
+		//	AccountOwner: "amin",
+		//	Sheba:        "IR0696000000010324200001",
+		//}
 
 		var card01 models.BankAccount
-		db.First(&card01, 2)
-		db.Save(account)
+		db.First(&card01, 1)
+		//db.Save(account)
 		return json.NewEncoder(c.Response()).Encode(card01)
 	})
 }
