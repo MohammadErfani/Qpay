235317b2674a9f0f4ccca7766b717c2cb1c76f61
o.moghadas <electromidz@gmail.com>
2023-11-17 19:03:35 +0330

    [WORK] work on validation on fields

--- server/routes/auth.go
+++ server/routes/auth.go
@@ -22,12 +22,18 @@ func AuthGroup(authG *echo.Group) {
 	authG.POST("/login", func(ctx echo.Context) error {
 
 		err := json.NewDecoder(ctx.Request().Body).Decode(&json_map)
+
 		email := json_map["email"].(string)
 		emailError := utils.IsValidEmail(email)
 		if emailError != nil {
-			return ctx.JSON(http.StatusBadRequest, "Email is not valid")
+			return ctx.JSON(http.StatusBadRequest, emailError)
 		}
-		// phoneNumber := json_map["phoneNumber"]
+
+		phoneNumber := json_map["phoneNumber"].(string)
+		phoneNumberError := utils.IsValidPhoneNumber(phoneNumber)
+		if phoneNumberError != nil {
+			return ctx.JSON(http.StatusBadRequest, phoneNumberError)
+		}
 		// password := json_map["password"]
 
 		if err != nil {
