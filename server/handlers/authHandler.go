e444960ae619fede7403a294f676c992210c4734
o.moghadas <electromidz@gmail.com>
2023-11-20 18:44:54 +0330

    [FIX] fix the error response in auth

--- server/handlers/authHandler.go
+++ server/handlers/authHandler.go
@@ -33,14 +33,14 @@ func (auth *Auth) Login(ctx echo.Context) error {
 	err := json.NewDecoder(ctx.Request().Body).Decode(&json_map)
 
 	if err != nil {
-		return ctx.JSON(http.StatusBadRequest, "Bad request")
+		return ctx.JSON(http.StatusBadRequest, map[string]string{"message": "Bad request"})
 	}
 
 	if json_map["email"] != nil {
 		loginReq.Email = json_map["email"].(string)
 		emailError := utils.IsValidEmail(loginReq.Email)
 		if emailError != nil {
-			return ctx.JSON(http.StatusBadRequest, emailError)
+			return ctx.JSON(http.StatusBadRequest, map[string]string{"hasError": "true", "message": emailError.Error()})
 		}
 	}
 
@@ -48,38 +48,37 @@ func (auth *Auth) Login(ctx echo.Context) error {
 		loginReq.PhoneNumber = json_map["phoneNumber"].(string)
 		phoneNumberError := utils.IsValidPhoneNumber(json_map["phoneNumber"].(string))
 		if phoneNumberError != nil {
-			return ctx.JSON(http.StatusBadRequest, phoneNumberError)
+			return ctx.JSON(http.StatusBadRequest, map[string]string{"hasError": "true", "message": phoneNumberError.Error()})
 		}
 	}
 
 	if json_map["password"] != nil {
 		loginReq.Password = json_map["password"].(string)
 		if len(loginReq.Password) == 0 {
-			return ctx.JSON(http.StatusBadRequest, "Input your password")
+			return ctx.JSON(http.StatusBadRequest, map[string]string{"hasError": "true", "message": "Please input your password!"})
 		}
 	} else {
-		return ctx.JSON(http.StatusBadRequest, "Input your password")
+		return ctx.JSON(http.StatusBadRequest, map[string]string{"hasError": "true", "message": "Please input your password!"})
 	}
 
 	user, errValidationUser := utils.GetUser(auth.DB, loginReq.Email, loginReq.PhoneNumber, loginReq.Password)
 
 	if errValidationUser != nil {
-		return ctx.String(http.StatusBadRequest, "Not valid Use!")
+		return ctx.JSON(http.StatusBadRequest, map[string]string{"hasError": "true", "message": "This user not valid!"})
 	}
 
 	if err != nil {
-		return ctx.String(http.StatusBadRequest, "Bad request")
+		return ctx.JSON(http.StatusBadRequest, map[string]string{"message": "Bad request"})
 	}
 
 	token, err := utils.CreateToken(auth.JWT, user)
 
 	if err != nil {
-		return ctx.String(http.StatusBadRequest, "Bad request")
+		return ctx.JSON(http.StatusBadRequest, map[string]string{"message": "Bad request"})
 	}
 
 	jsonToken := new(LoginRes)
 	jsonToken.Token = token
 
 	return ctx.JSON(http.StatusOK, jsonToken)
-
 }
